fbe89710f40bcf752657be8783fd79653cde0e4c
kdewilde <kevin@dewil.de>
2019-05-01 10:22:54 +0200

    Updated lets split for RGB to work

--- keyboards/lets_split/keymaps/me/rules.mk
+++ keyboards/lets_split/keymaps/me/rules.mk
@@ -1,3 +1,2 @@
-ifndef QUANTUM_DIR
-	include ../../../../Makefile
-endif
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
+EXTRAKEY_ENABLE = yes       # Audio control and System control(+450)
